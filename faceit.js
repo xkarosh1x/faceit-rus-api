0cc8014522a5925d9cb00b7194f6e7efd218e891
xkarosh1x <frencesborn@gmail.com>
2026-03-18 03:04:59 +0300

    Add files via upload

---
+++ faceit.js
@@ -0,0 +1,31 @@
+// api/faceit.js
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  
+  const { nick } = req.query;
+  
+  if (!nick) {
+    return res.status(400).json({ error: 'Укажи ник: ?nick=xKarosh1x' });
+  }
+
+  const FACEIT_API_KEY = 'cf9af14a-245b-4d36-80e4-721625d0532a'; // Вставь свой ключ
+  
+  try {
+    const response = await fetch(
+      `https://open.faceit.com/data/v4/players?nickname=${nick}`,
+      {
+        headers: {
+          'Authorization': `Bearer ${FACEIT_API_KEY}`
+        }
+      }
+    );
+    
+    const data = await response.json();
+    
+    // Временный вывод — показываем ВСЁ, что пришло
+    return res.status(200).json(data.games.cs2);
+    
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
